992d9b444a88285410cf4302b698fcc2066511d2
Tanner Nelson <me@tanner.xyz>
2017-11-21 14:50:07 -0500

    implement future stream(to:)

--- Sources/Async/Futures/Future+Stream.swift
+++ Sources/Async/Futures/Future+Stream.swift
@@ -5,9 +5,6 @@ extension Future {
     ///
     /// [Learn More →](https://docs.vapor.codes/3.0/async/streams-basics/#chaining-streams_1)
     public func stream<S: InputStream>(to stream: S) where S.Input == Expectation {
-        // FIXME
-//        self.do(stream.input).catch { error in
-//            // stream.report(error)
-//        }
+        self.do(stream.onInput).catch(stream.onError)
     }
 }

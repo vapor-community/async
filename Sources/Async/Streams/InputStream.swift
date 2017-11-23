4b3d7eb8ea0f3f04a3562ae31f7d34997101eb40
Tanner Nelson <me@tanner.xyz>
2017-11-22 21:12:02 -0500

    all streams closable

--- Sources/Async/Streams/InputStream.swift
+++ Sources/Async/Streams/InputStream.swift
@@ -1,7 +1,7 @@
 /// A type that accepts a stream of `Input`
 ///
 /// [Learn More →](https://docs.vapor.codes/3.0/async/streams-introduction/#implementing-an-example-stream)
-public protocol InputStream: BaseStream {
+public protocol InputStream: ClosableStream {
     /// The input type for this stream.
     /// For example: Request, ByteBuffer, Client
     associatedtype Input
--- Sources/Async/Streams/OutputStream.swift
+++ Sources/Async/Streams/OutputStream.swift
@@ -1,7 +1,7 @@
 /// A type that emits `Ouptut` asynchronously and at unspecified moments
 ///
 /// [Learn More →](https://docs.vapor.codes/3.0/async/streams-introduction/#implementing-an-example-stream)
-public protocol OutputStream: BaseStream {
+public protocol OutputStream: ClosableStream {
     /// The output type for this stream.
     /// For example: Request, ByteBuffer, Client
     associatedtype Output
@@ -18,11 +18,7 @@ extension OutputStream {
     /// [Learn More →](https://docs.vapor.codes/3.0/async/streams-introduction/#draining-streams)
     public func drain(onInput: @escaping BasicStream<Output>.OnInput) -> BasicStream<Output> {
         let input = BasicStream<Output>(onInput: onInput)
-        self.onOutput(input)
-        if let notifier = self as? ClosableStream {
-            notifier.onClose(input)
-        }
-        return input
+        return self.stream(to: input)
     }
 
 
@@ -34,18 +30,7 @@ extension OutputStream {
     @discardableResult
     public func stream<S>(to stream: S) -> S where S: InputStream, S.Input == Self.Output {
         self.onOutput(stream)
-        if let notifier = self as? ClosableStream {
-            if let listener = stream as? ClosableStream {
-                notifier.onClose(listener)
-            } else {
-                /// FIXME: make all streams closable?
-                print("""
-                [⚠️ Dev Warning] Closable stream connected to non-closable stream.
-                \(Self.self) -> \(S.self).
-                On close events will not be propogated.
-                """)
-            }
-        }
+        self.onClose(stream)
         return stream
     }
 }

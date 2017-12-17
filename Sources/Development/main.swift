f2e3f9c02733ccc7e2d57a4569bbe524b0e26d47
Tanner Nelson <me@tanner.xyz>
2017-12-16 23:10:45 -0500

    fix spacing

--- Sources/Development/main.swift
+++ Sources/Development/main.swift
@@ -24,42 +24,41 @@ func test<EventLoop>(_ type: EventLoop.Type) throws
         }
     }
 
-    let tcpSocket = try TCPSocket(isNonBlocking: true, shouldReuseAddress: false)
+    let tcpSocket = try TCPSocket(isNonBlocking: true, shouldReuseAddress: true)
     let tcpServer = try TCPServer(socket: tcpSocket)
     try tcpServer.start(hostname: "localhost", port: 8123, backlog: 128)
     let acceptStream = tcpServer.stream(on: accept)
 
     var response = Data("HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n".utf8)
+    let responseBuffer = response.withUnsafeBytes { (bytes: UnsafePointer<UInt8>) in
+        return UnsafeBufferPointer<UInt8>(start: bytes, count: response.count)
+    }
 
     acceptStream.drain { upstream in
         upstream.request(count: .max)
-        }.output { client in
-            loopOffset += 1
-            if loopOffset >= loops.count {
-                loopOffset = 0
-            }
-            let stream = client.stream(on: loops[loopOffset])
-            var upstream: ConnectionContext?
-            stream.drain { context in
-                upstream = context
-                context.request(count: 1)
-                }
-                .output { buffer in
-                    //                stream.next(buffer)
-                    response.withUnsafeBytes { (bytes: UnsafePointer<UInt8>) in
-                        let buffer = UnsafeBufferPointer<UInt8>(start: bytes, count: response.count)
-                        stream.next(buffer)
-                    }
-                    upstream?.request(count: 1)
-                }.catch { error in
-                    print("\(error)")
-                }.finally {
-                    print("CLOSED")
-            }
+    }.output { client in
+        loopOffset += 1
+        if loopOffset >= loops.count {
+            loopOffset = 0
+        }
+        let stream = client.stream(on: loops[loopOffset])
+        var upstream: ConnectionContext?
+        stream.drain { context in
+            upstream = context
+            context.request(count: 1)
+        }
+        .output { buffer in
+            stream.next(responseBuffer)
+            upstream?.request(count: 1)
         }.catch { error in
             print("\(error)")
         }.finally {
             print("CLOSED")
+        }
+    }.catch { error in
+        print("\(error)")
+    }.finally {
+        print("CLOSED")
     }
 
     accept.run()

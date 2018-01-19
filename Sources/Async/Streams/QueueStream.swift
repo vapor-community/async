51dab8e056b8995ded0f3010b39ce74cce8ee60e
Tanner Nelson <me@tanner.xyz>
2018-01-18 21:15:31 -0500

    queue stream impl

--- Sources/Async/Streams/QueueStream.swift
+++ Sources/Async/Streams/QueueStream.swift
@@ -1,94 +1,129 @@
-/// Allows for input to be enqueued by a push stream or user.
-/// The next output will be returned as a Future for enqueued inputs.
+/// Enqueues a single input and waits for multiple output.
+/// This is useful for situations where one request can lead
+/// to multiple responses.
 public final class QueueStream<I, O>: Stream, ConnectionContext {
-    /// See InputStream.Input
+    /// See `InputStream.Input`
     public typealias Input = I
 
-    /// See OutputStream.Output
+    /// See `OutputStream.Output`
     public typealias Output = O
 
-    /// Queue of promised responses
-    var inputQueue: [Promise<Input>]
+    /// Current upstream output stream.
+    private var upstream: ConnectionContext?
 
-    /// Queue of requests to be serialized
-    var outputQueue: [Output]
+    /// Current downstrema input stream.
+    private var downstream: AnyInputStream<Output>?
 
-    /// Accepts serialized requests
-    var downstream: AnyInputStream<Output>?
+    /// Current downstream demand.
+    private var downstreamDemand: UInt
 
-    /// Serialized requests
-    var remainingDownstreamRequests: UInt
+    /// Queued output.
+    private var queuedOutput: [Output]
 
-    /// Parsed responses
-    var upstream: ConnectionContext?
+    /// Queued input.
+    private var queuedInput: [QueueStreamInput<Input>]
 
-    /// Creates a new `QueueStream` stream
+    /// Current input being handled.
+    private var currentInput: QueueStreamInput<Input>?
+
+    /// Create a new `AsymmetricQueueStream`.
     public init() {
-        self.inputQueue = []
-        self.outputQueue = []
-        self.remainingDownstreamRequests = 0
+        self.downstreamDemand = 0
+        self.queuedOutput = []
+        self.queuedInput = []
     }
 
-    /// Enqueues a new output to the stream and returns
-    /// a future that will be completed with the next input.
-    public func enqueue(_ output: Output) -> Future<Input> {
-        let promise = Promise(Input.self)
-        self.outputQueue.append(output)
-        self.inputQueue.append(promise)
-        upstream?.request()
+    /// Enqueue the supplied output, specifying a closure that will determine
+    /// when the Input received is ready.
+    public func enqueue(_ output: [Output], onInput: @escaping (Input) throws -> Bool) -> Future<Void> {
+        let input = QueueStreamInput(onInput: onInput)
+        self.queuedInput.insert(input, at: 0)
+        for o in output {
+            self.queuedOutput.insert(o, at: 0)
+        }
+        upstream!.request(count: 1)
         update()
-        return promise.future
+        return input.promise.future
     }
 
-    @available(*, deprecated, renamed: "enqueue")
-    public func queue(_ output: Output) -> Future<Input> {
-        return enqueue(output)
-    }
-
-    /// Updates the stream's state. If there are outstanding
-    /// downstream requests, they will be fulfilled.
-    func update() {
-        guard remainingDownstreamRequests > 0 else {
-            return
-        }
-        while let output = outputQueue.popLast() {
-            remainingDownstreamRequests -= 1
-            downstream?.next(output)
+    /// Enqueue the supplied output, returning a Future that will be completed
+    /// with the first received Input.
+    public func enqueue(_ output: Output) -> Future<Input> {
+        var capturedInput: Input?
+        return enqueue([output]) { input in
+            capturedInput = input
+            return true
+        }.map(to: Input.self) {
+            return capturedInput!
         }
     }
 
-    /// See ConnectionContext.connection
+    /// Updates internal state.
+    private func update() {
+        while downstreamDemand > 0 {
+            guard let output = queuedOutput.popLast() else {
+                break
+            }
+            downstreamDemand -= 1
+            downstream!.next(output)
+        }
+    }
+
+    /// See `ConnectionContext.connection`
     public func connection(_ event: ConnectionEvent) {
         switch event {
+        case .cancel: break // handle better
         case .request(let count):
-            let isSuspended = remainingDownstreamRequests == 0
-            remainingDownstreamRequests += count
-            if isSuspended { update() }
-        case .cancel:
-            /// FIXME: better cancel support
-            remainingDownstreamRequests = 0
+            downstreamDemand += count
+            update()
         }
     }
 
-    /// See OutputStream.output
-    public func output<S>(to inputStream: S) where S : InputStream, S.Input == Output {
-        downstream = AnyInputStream(inputStream)
-        inputStream.connect(to: self)
-    }
-
-    /// See InputStream.input
-    public func input(_ event: InputEvent<Input>) {
+    /// See `InputStream.input`
+    public func input(_ event: InputEvent<I>) {
         switch event {
+        case .close: downstream?.close()
         case .connect(let upstream):
             self.upstream = upstream
-            upstream.request(count: UInt(inputQueue.count))
-        case .next(let input):
-            let promise = inputQueue.popLast()!
-            promise.complete(input)
             update()
         case .error(let error): downstream?.error(error)
-        case .close:
-            downstream?.close()
+        case .next(let input):
+            var context: QueueStreamInput<Input>
+            if let current = currentInput {
+                context = current
+            } else {
+                let next = queuedInput.popLast()!
+                currentInput = next
+                context = next
+            }
+
+            do {
+                if try context.onInput(input) {
+                    currentInput = nil
+                    context.promise.complete()
+                } else {
+                    upstream!.request(count: 1)
+                }
+            } catch {
+                currentInput = nil
+                context.promise.fail(error)
+            }
         }
+    }
+
+    /// See `OutputStream.output`
+    public func output<S>(to inputStream: S) where S : InputStream, S.Input == Output {
+        downstream = .init(inputStream)
+        inputStream.connect(to: self)
+    }
+}
+
+struct QueueStreamInput<Input> {
+    var promise: Promise<Void>
+    var onInput: (Input) throws -> Bool
+
+    init(onInput: @escaping (Input) throws -> Bool) {
+        self.promise = .init()
+        self.onInput = onInput
     }
 }

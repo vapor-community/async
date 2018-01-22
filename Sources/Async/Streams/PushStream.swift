8fd716c82913b70475faf388ea780e1db077bf72
Tanner Nelson <me@tanner.xyz>
2018-01-22 00:23:59 -0500

    pre-loaded push stream

--- Sources/Async/Streams/PushStream.swift
+++ Sources/Async/Streams/PushStream.swift
@@ -5,26 +5,35 @@ public final class PushStream<Pushing>: Stream {
     var backlog: [Pushing]
     var downstream: AnyInputStream<Pushing>?
     var isReady: Bool
-    
+    var isClosed: Bool
+
     public init(_ type: Pushing.Type = Pushing.self) {
         backlog = []
         isReady = true
+        isClosed = false
     }
 
     /// Pushes a new `Input` into the stream.
     public func push(_ input: Input) {
-        if isReady {
+        if isReady, let downstream = self.downstream {
             isReady = false
-            downstream!.next(input).do {
+            downstream.next(input).do {
                 self.isReady = true
-                if let last = self.backlog.popLast() {
-                    self.push(last)
-                }
+                self.update()
             }.catch { error in
-                self.downstream?.error(error)
+                downstream.error(error)
             }
         } else {
             backlog.insert(input, at: 0)
+        }
+    }
+
+    /// Checks to see if additional work needs to be done.
+    private func update() {
+        if let last = self.backlog.popLast() {
+            self.push(last)
+        } else if isClosed {
+            downstream?.close()
         }
     }
 
@@ -32,7 +41,10 @@ public final class PushStream<Pushing>: Stream {
     public func input(_ event: InputEvent<Input>) {
         switch event {
         case .close:
-            downstream?.close()
+            isClosed = true
+            if backlog.count == 0 {
+                downstream?.close()
+            }
         case .error(let error):
             downstream?.error(error)
         case .next(let input, let ready):
@@ -40,9 +52,11 @@ public final class PushStream<Pushing>: Stream {
             ready.complete() // input stream can immidately complete since we are backlogging input
         }
     }
-    
+
     /// See `OutputStream.output`
     public func output<S>(to inputStream: S) where S : InputStream, Output == S.Input {
         downstream = AnyInputStream(inputStream)
+        update()
     }
 }
+
